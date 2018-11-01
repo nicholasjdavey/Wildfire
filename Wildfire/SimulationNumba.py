d585b91c4e5e469b125a1c0e4e14228078c06945
nicholasjdavey <nicholasjdavey@gmail.com>
2018-11-01 11:12:57 +1100

    CUDA is so much faster. Now to build the heuristic.

--- Wildfire/SimulationNumba.py
+++ Wildfire/SimulationNumba.py
@@ -31,6 +31,7 @@ def simulateSinglePath(paths, totalSteps, lookahead, sampleFFDIs,
 
     if path < paths:
         expectedE = cuda.local.array(shape=16000, dtype=float32)
+        expectedP = cuda.local.array(shape=16000, dtype=float32)
 
         for tt in range(totalSteps):
             expectedFFDI = sampleFFDIs[:, tt:(totalSteps + lookahead + 1)]
@@ -41,34 +42,7 @@ def simulateSinglePath(paths, totalSteps, lookahead, sampleFFDIs,
                     occurrence, configsE, lookahead, expectedE, rng_states,
                     path)
 
-##@jit(nopython=True)
-#@cuda.jit
-#def simulateSinglePath(sampleFFDIs, patchVegetations, patchLocations,
-#                       ffdiRanges, rocA2PHMeans, rocA2PHSDs, occurrence,
-#                       resourceTypes, resourceSpeeds, configurations, configsE,
-#                       configsP, totalSteps, lookahead, accumulatedDamages,
-#                       accumulatedHours, fires, fireSizes, fireLocations,
-#                       firePatches, aircraftLocations, aircraftAssignments,
-#                       expectedE, expectedP):
-#
-#    # ThreadID
-#    tx = cuda.threadIdx.x
-#    # BlockID
-#    ty = cuda.blockIdx.x
-#    # BlockWidth
-#    bw = cuda.blockDim.x
-#    # GlobalID (pathNo)
-#    pos = tx + ty * bw
-#
-#    for tt in range(totalSteps):
-#        expectedFFDI = sampleFFDIs[:, tt:(totalSteps + lookahead + 1)]
-#
-##        expectedDamageExisting(
-##                expectedFFDI, firePatches[tt], fireSizes[tt],
-##                patchVegetations, ffdiRanges, rocA2PHMeans, rocA2PHSDs,
-##                occurrence, configsE, lookahead, expectedE)
-#
-##        expDamagePotential = expectedDamagePotential()
+            expDamagePotential = expectedDamagePotential()
 #    #     assignAircraft
 #    #     simulateNextStep:
 
@@ -99,9 +73,39 @@ def expectedDamageExisting(ffdi_path, fire_patches, fire_sizes,
             expected[fire*len(configs) + config] = max(
                     0, size - fire_sizes[fire])
 
-@jit(nopython=True)
+@cuda.jit(device=True)
 def expectedDamagePotential():
-    pass
+
+    for patch in range(len(patches)):
+        damage = 0
+        vegetation = int(patch_vegetatios[patch])
+        ffdi_range = ffdi_ranges[vegetation]
+        roc_a2_ph_mean = roc_a2_ph_means[vegetation]
+        roc_a2_ph_sd = roc_a2_ph_sds[vegetation]
+        occur_veg = occurrence[vegetation]
+
+        for tt in range(lookahead):
+            # Only look at the expected damage of fires started at this time
+            # period to the end of the horizon
+            occ = max(0, interpolate1D(ffdi, ffdi_range, occur_veg[time + tt]))
+            size = interpolate1D(ffdi, ffdi_range, initial_size[config])
+            success = interpolate1D(ffdi, ffdi_range, initial_success[config])
+
+            sizeInitial = size
+
+            for t2 in range(tt, lookahead):
+                ffdi = ffdi_path[patch, t2]
+
+                gr_mean = interpolate1D(ffdi, ffdi_range,
+                                        roc_a2_ph_mean[config])
+
+                radCurr = (math.sqrt(size*10000.0/math.pi))
+                radNew = radCurr + max(0, gr_mean)
+                size = (math.pi * radNew**2)/10000.0
+
+            damage += occ * size * (1 - success) + occ*sizeInitial*success
+
+            expected[patch * len(configs) + config] = damage*patch_areas[patch]
 
 #@jit(nopython=True, fastmath=True)
 @cuda.jit(device=True)
